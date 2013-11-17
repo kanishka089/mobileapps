4add671a19796f0428731d15a9b912ca0b89c816
Kanishka <kanishka.gunawardana@gmail.com>
2013-11-17 19:55:43 +0530

    7:55 pm 17/11/2013

--- SuperDiary/gen/com/diary/superdiary/R.java
+++ SuperDiary/gen/com/diary/superdiary/R.java
@@ -22,12 +22,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int images=0x7f020001;
+        public static final int index=0x7f020002;
+        public static final int kyg=0x7f020003;
+        public static final int kygk=0x7f020004;
+        public static final int lee=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080001;
+        public static final int imageView1=0x7f080000;
+        public static final int pass=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int home=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- SuperDiary/src/com/diary/superdiary/MainActivity.java
+++ SuperDiary/src/com/diary/superdiary/MainActivity.java
@@ -2,22 +2,45 @@ package com.diary.superdiary;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity{
+	 EditText pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        Button enter=(Button) findViewById(R.id.button1);
+        pass=(EditText) findViewById(R.id.pass);
+        final Toast error=Toast.makeText(this,"Invalid Login", Toast.LENGTH_SHORT);
+        enter.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(pass.getText().toString().equals("kani")){
+					startActivity(new Intent("com.diary.superdiary.login"));
+				}
+				else{
+					error.show();
+				}
+				
+			}
+		});
+        
+                
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
     
 }
